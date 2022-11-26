f9690a4de4e6333cf1e14e8cd07d608e577bca48
Madeleine Hill <mh11wi@outlook.com>
2022-11-25 21:47:30 -0500

    Update jest config Test was previously failing after react-disks import.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the Sum Disks app bar', () => {
   render(<App />);
   const element = screen.getByText(/Sum Disks/i);
   expect(element).toBeInTheDocument();
