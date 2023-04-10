24f8323b5f29de9376de9f6cd4569df99d90b5eb
Madeleine Hill <mh11wi@outlook.com>
2023-04-10 19:13:55 -0400

    Debounce rotate callback Instead of delaying win check, debounce the whole rotate callback so that if the user double clicks past the correct answer, the win is not counted (and then uncounted).

--- src/App.js
+++ src/App.js
@@ -35,6 +35,16 @@ const adStyle = {
   margin: '0.5rem'
 };
 
+function debounce(func, timeout) {
+  let timer;
+  return function(event) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(func, timeout, event);
+  };
+}
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -163,8 +173,8 @@ function App() {
   }, [hasWon]);
   
   const onRotate = (rotatedDisksText) => {
-    setRotatedDisksText((rotatedDisksText));
-    setTimeout(() => setHasWon(isSolved(sum, rotatedDisksText)), 500);
+    setRotatedDisksText(rotatedDisksText);
+    setHasWon(isSolved(sum, rotatedDisksText));
   }
   
   const handleClickNewGame = () => {
@@ -243,7 +253,7 @@ function App() {
             <ReactDisks 
               disksText={disksText}
               theme={theme.palette.primary}
-              onRotate={onRotate}
+              onRotate={debounce(onRotate, 500)}
               disabled={hasWon}
             />
           </Box>
