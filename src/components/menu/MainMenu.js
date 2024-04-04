3b8c40aeda3405a83ffef12ddd23a43dcdb3b238
Madeleine Hill <mh11wi@outlook.com>
2024-04-04 01:35:07 -0400

    UI Updates - add style game interface for xl breakpoint - install new react-disks for future dark mode setting, and improved swipe handling - make mode selection and challenge creation/starting experience more intuitive

--- src/components/menu/MainMenu.js
+++ src/components/menu/MainMenu.js
@@ -64,8 +64,12 @@ const MainMenu = (props) => {
   }
   
   const handleChangeMode = (event) => {
-    props.onClose();
-    props.handleChangeMode(event);
+    const selected = event.target.closest('.modeOption')?.getAttribute('data-value');
+    
+    if (selected) {
+      props.onClose();
+      props.handleChangeMode(event);
+    }
   }
   
   return (
@@ -75,7 +79,8 @@ const MainMenu = (props) => {
           width: {
             xs: '50%',
             sm: '33%',
-            lg: '20%'
+            lg: '20%',
+            xl: '17.5%'
           }
         }
       }}
@@ -92,19 +97,19 @@ const MainMenu = (props) => {
                 id="game-mode"
                 label="Game Mode"
                 defaultValue={gameMode}
-                onChange={handleChangeMode}
+                onClose={handleChangeMode}
                 sx={{ 
                   '.MuiListItemIcon-root': { minWidth: 'auto', pr: 2 },
                   '.MuiSelect-select': { display: "flex !important", alignItems: 'center' },
                 }}
               >
-                <MenuItem value="unlimited">
+                <MenuItem className="modeOption" value="unlimited">
                   <ListItemIcon>
                     <AllInclusive />
                   </ListItemIcon>
                   <ListItemText>Unlimited Mode</ListItemText>
                 </MenuItem>
-                <MenuItem value="challenge">
+                <MenuItem className="modeOption" value="challenge">
                   <ListItemIcon>
                     <Grade />
                   </ListItemIcon>
